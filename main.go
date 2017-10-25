839b658c5b9b59edf749d12e0cd49f7fb7448e56
didacat <33058928+didacat@users.noreply.github.com>
2017-10-25 17:36:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -57,9 +57,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				/*if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK2!")).Do(); err != nil {
-					log.Print(err)
-				}*/
 				if message.Text == "/vote"{
 					log.Print("Start Vote")
 					bot.PushMessage(groupID, linebot.NewTextMessage("Start Vote!")).Do()
