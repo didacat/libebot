bc4e71264fde6395a41090a1d05d0145102c1234
didacat <33058928+didacat@users.noreply.github.com>
2017-10-27 16:14:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					bot.PushMessage(
 						groupID, 
 						linebot.NewImageMessage(
-						"https://github.com/didacat/linebot/images/",
+						"https://github.com/didacat/linebot/blob/master/images/linebotH1.png",
 						"Imagemap alt text"	,	
 						)		,		
 					).Do(); 
