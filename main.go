0660032b32e5e5d2989196e0957d58c00ab0af89
didacat <33058928+didacat@users.noreply.github.com>
2017-11-03 16:24:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"math/rand"
 	"strconv"  
+	"strings"
 	"time"  
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -17,14 +18,20 @@ var bot *linebot.Client
 var port =""
 var addr =""
 var isGameStart bool = false
-var UserNameSlice []string
-var UserIDSlice []string
-var UserAnsMap = make(map[string]string)
-var UserCanSpeakSlice []bool
-var WhoRound int = 0
+var UserNameSlice []string	//玩家名稱
+var UserIDSlice []string	//玩家ID
+var UserAnsMap = make(map[string]string) //玩家ID跟骰子數值的MAP表
+var UserCanSpeakSlice []bool //玩家是否能說話
+var UserDiceCount []int //玩家的骰子數量
+var WhoRound int = 0	//輪到誰的INDEX
 var m_groupID =""
 
 var test = 6
+
+type DiceValue struct {	
+	Values []string
+}
+ 
 func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
@@ -76,14 +83,36 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					}
 					
 					if(message.Text =="1"){
-						UserAnser = "單雙"
-						test = test - 1
+						UserAnser = "單"
+						NewValue := ""
+						for _, value := range UserIDSlice {
+							
+							NewValue = strings.TrimRight(UserAnsMap[value],"1")
+							NewValue = strings.TrimRight(UserAnsMap[value],"3")
+							NewValue = strings.TrimRight(UserAnsMap[value],"5")
+							// for _, DiceValue := range UserAnsMap[value] {
+							// 	if(strings.EqualFold(DiceValue,"2") || strings.EqualFold(DiceValue,"4") || strings.EqualFold(DiceValue,"6")){
+							// 		NewValue += DiceValue
+							// 	}
+							// }
+							UserAnsMap[value] = NewValue
+							
+						}
 					}else if(message.Text =="2"){
-						UserAnser = "大小"
+						UserAnser = "雙"
 						test = test - 2
 					}else if(message.Text =="3"){
-						UserAnser = "紅黑"
+						UserAnser = "大"
 						test = test - 3
+					}else if(message.Text =="4"){
+						UserAnser = "小"
+						test = test - 2
+					}else if(message.Text =="5"){
+						UserAnser = "紅"
+						test = test - 3
+					}else if(message.Text =="6"){
+						UserAnser = "黑"
+						test = test - 2
 					}
 
 
@@ -94,6 +123,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						WhoRound += 1
 					}
 					
+					//刪除 MAP 表
+					// for _, value := range UserIDSlice {
+					// 	delete(UserAnsMap,value)
+					// }
+					
 					for _, value := range UserIDSlice {
 						rand.Seed(time.Now().UnixNano())  						
 						SliceValue := make([]int, test)
@@ -103,7 +137,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							element = element + 1 
 							NumerString = NumerString + strconv.Itoa(element)  
 						}
-						UserAnsMap[NumerString] = value
+						// UserAnsMap[NumerString] = value
 						log.Print("NumerString = " + NumerString)
 						log.Print(UserAnsMap)
 						log.Print(value)
@@ -117,7 +151,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						).Do(); 
 					}
 					//讓第一位玩家 可以回答
-					bot.PushMessage(UserIDSlice[WhoRound], linebot.NewTextMessage("請決定你要喊的骰子\n 1)單雙 \n 2)大小 \n 3)紅黑" )).Do()
+					bot.PushMessage(UserIDSlice[WhoRound], linebot.NewTextMessage("請決定你要喊的骰子\n 1)單 \n 2)雙 \n 3)大\n 4)小 \n 5)紅 \n 6)黑" )).Do()
 					//發給群組 現在是誰的回合
 					// bot.PushMessage(groupID, linebot.NewTextMessage("現在是 " +  UserNameSlice[WhoRound] + "的回合")).Do()
 
@@ -144,6 +178,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							UserName := message.Text[6:len(message.Text)]
 							UserNameSlice= append(UserNameSlice, UserName)	//紀錄玩家輸入的名稱 之後推撥會顯示玩家名稱的回合
 							UserIDSlice = append(UserIDSlice, userID)	//紀錄玩家的ID 便於後續發送圖片
+							UserDiceCount = append(UserDiceCount, 6)	//初始都給玩家 6顆骰子
 							UserCanSpeakSlice = append(UserCanSpeakSlice, false) //玩家有無回答的權限
 							TotalUser := ""
 							for _, value := range UserNameSlice {
@@ -179,7 +214,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								element = element + 1 
 								NumerString = NumerString + strconv.Itoa(element)  
 							}
-							UserAnsMap[NumerString] = value
+							UserAnsMap[value] = NumerString
 							log.Print("NumerString = " + NumerString)
 							log.Print(UserAnsMap)
 							log.Print(value)
@@ -193,7 +228,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							).Do(); 
 						}
 						//讓第一位玩家 可以回答
-						bot.PushMessage(UserIDSlice[0], linebot.NewTextMessage("請決定你要喊的骰子\n 1)單雙 \n 2)大小 \n 3)紅黑" )).Do()
+						bot.PushMessage(UserIDSlice[WhoRound], linebot.NewTextMessage("請決定你要喊的骰子\n 1)單 \n 2)雙 \n 3)大\n 4)小 \n 5)紅 \n 6)黑" )).Do()
 						//發給群組 現在是誰的回合
 						bot.PushMessage(groupID, linebot.NewTextMessage("現在是 " +  UserNameSlice[0] + "的回合")).Do()
 						
