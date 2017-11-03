be6d8dae2f9ad3960e7b25a46cb90086b6d77646
didacat <33058928+didacat@users.noreply.github.com>
2017-11-03 13:50:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var isGameStart bool = false
 var UserNameSlice []string
 var UserIDSlice []string
 var UserAnsMap = make(map[string]string)
+var UserCanSpeakSlice []bool
 func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
@@ -62,7 +63,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print("Start DiceGame")
 					bot.PushMessage(groupID, linebot.NewTextMessage("Start DiceGame!")).Do()
 					isGameStart = true
-				}else if message.Text == "/stopdice" && isGameStart == true {
+				}else if message.Text == "/dicestop" && isGameStart == true {
 					log.Print("Stop DiceGame")
 					UserNameSlice = UserNameSlice[:0]
 					UserIDSlice = UserIDSlice[:0]
@@ -78,8 +79,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							log.Print(err)
 						}
 						UserName := message.Text[6:len(message.Text)]
-						UserNameSlice= append(UserNameSlice, UserName)
-						UserIDSlice = append(UserIDSlice, userID)
+						UserNameSlice= append(UserNameSlice, UserName)	//紀錄玩家輸入的名稱 之後推撥會顯示玩家名稱的回合
+						UserIDSlice = append(UserIDSlice, userID)	//紀錄玩家的ID 便於後續發送圖片
+						UserCanSpeakSlice = append(UserCanSpeakSlice, false) //玩家有無回答的權限
 						TotalUser := ""
 						for _, value := range UserNameSlice {
 							TotalUser += value + ","
@@ -106,7 +108,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print("dicestart Receive")
 					
 					for _, value := range UserIDSlice {
-
 						rand.Seed(time.Now().UnixNano())  
 						arrValue := [...]int{1,2,3,4,5,6}
 						NumerString := ""
@@ -119,6 +120,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						log.Print("NumerString = " + NumerString)
 						log.Print(UserAnsMap)
 						log.Print(value)
+						//發送給玩家圖片
 						bot.PushMessage(
 							value, 
 							linebot.NewImageMessage(
@@ -127,6 +129,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								)		,	
 						).Do(); 
 					}
+					//讓第一位玩家 可以回答
+					bot.PushMessage(UserIDSlice[0], linebot.NewTextMessage("請決定你要喊的骰子\n 1)單雙 \n 2)大小 \n 3)紅黑" )).Do()
+					//發給群組 現在是誰的回合
+					bot.PushMessage(groupID, linebot.NewTextMessage("現在是 " +  UserNameSlice[0] + "的回合")).Do()
 					
 				}
 				log.Print(event.ReplyToken)
