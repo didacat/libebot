ba8e6b9dbd19ec3cc7bc871743557eccace658f8
didacat <33058928+didacat@users.noreply.github.com>
2017-11-07 10:07:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -411,8 +411,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								}
 
 								TotalMsg := ""
-								for i, _ := range UserNameSlice {
-									TotalMsg = UserNameSlice[i] + " = " + UserAnsMap[UserNameSlice[i]]
+								for i, value := range UserNameSlice {
+									TotalMsg = value + " = " + UserAnsMap[UserIDSlice[i]] + "\n"
 								}
 								bot.PushMessage(m_groupID, linebot.NewTextMessage("所有玩家的牌面是 : \n "+TotalMsg+"\n 總共有"+strconv.Itoa(AllDiceValueAndCount[UserSpeakDiceValue])+"個"+strconv.Itoa(UserSpeakDiceValue))).Do()
 							} else { //沒有喊過1的情況
@@ -424,8 +424,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								}
 
 								TotalMsg := ""
-								for i, _ := range UserNameSlice {
-									TotalMsg = UserNameSlice[i] + " = " + UserAnsMap[UserNameSlice[i]] + "\n"
+								for i, value := range UserNameSlice {
+									TotalMsg = value + " = " + UserAnsMap[UserIDSlice[i]] + "\n"
 								}
 								bot.PushMessage(m_groupID, linebot.NewTextMessage("所有玩家的牌面是 : \n "+TotalMsg+"\n 總共有"+strconv.Itoa(AllDiceValueAndCount[UserSpeakDiceValue])+"個"+strconv.Itoa(UserSpeakDiceValue))).Do()
 							}
